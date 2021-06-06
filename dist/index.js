bc2b2603fc48d674ab8307c3a6581c26e6f60fbb
martin <martin@anemona.com>
2021-06-06 15:01:13 -0500

    the project documentation and source code were updated

--- dist/index.js
+++ dist/index.js
@@ -1,245 +1,70 @@
-'use strict';
-
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
-var _aifCipher = require('aif-cipher');
-
-var _aifStr = require('aif-str');
-
-var _path = require('path');
-
-var _path2 = _interopRequireDefault(_path);
-
-var _fs = require('fs');
-
-var _fs2 = _interopRequireDefault(_fs);
-
-var _shelljs = require('shelljs');
-
-var _shelljs2 = _interopRequireDefault(_shelljs);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-module.exports = function () {
-  function DataBase(dir, name, key, table) {
-    _classCallCheck(this, DataBase);
-
-    this.key = key || null;
-    this.id = null;
-    this.name = this.hash(name);
-    this.path = _path2.default.join(dir, this.name);
-    this.table = table || 'table';
-    this.lastError = '';
-    if (!_fs2.default.existsSync(this.path)) {
-      _shelljs2.default.mkdir('-p', this.path);
-    }
-  }
-
-  DataBase.prototype.hash = function hash(data) {
-    return this.key ? (0, _aifCipher.md5)(data + this.key) : data;
-  };
-
-  DataBase.prototype.idHash = function idHash(id) {
-    id = id || this.id;
-    if (id) {
-      this.id = id;
-      return this.hash(id);
-    }
-    return null;
-  };
-
-  DataBase.prototype.dir = function dir(id, table) {
-    id = id || this.id;
-    if (id) {
-      this.id = id;
-      table = table || this.table;
-      table = this.hash(table);
-      return _path2.default.join(this.path, table) + _path2.default.sep + (0, _aifStr.split)(id + '', 2).join(_path2.default.sep);
-    }
-    return null;
-  };
-
-  DataBase.prototype.file = function file(_file, id) {
-    id = id || this.id;
-    _file = _file || this.table;
-    _file = this.hash(_file);
-    if (id) {
-      this.id = id;
-      return this.dir(id) + _path2.default.sep + _file;
-    }
-    return null;
-  };
-
-  DataBase.prototype.saveFile = function saveFile(content, file) {
-    if (_fs2.default.existsSync(content)) {
-      content = _fs2.default.readFileSync(content, { encoding: 'utf8' });
-    }
-    content = content || '';
-    file = file || this.table;
-    file = this.hash(file);
-    var error = null;
-    try {
-      content = (typeof content === 'undefined' ? 'undefined' : _typeof(content)) === 'object' ? JSON.stringify(content) : content;
-      content = this.key ? _aifCipher.Aes.encode(content, this.key) : content;
-      file = this.dir() + _path2.default.sep + file;
-      _fs2.default.writeFileSync(file, content, { encoding: 'utf8' });
-    } catch (err) {
-      this.lastError = err.toString();
-      error = err;
-    }
-    return !!error;
-  };
-
-  DataBase.prototype.loadFile = function loadFile(file) {
-    file = file || this.table;
-    file = this.hash(file);
-    file = this.dir() + _path2.default.sep + file;
-    var content = null;
-    var error = null;
-    if (_fs2.default.existsSync(file)) {
-      try {
-        content = _fs2.default.readFileSync(file, { encoding: 'utf8' });
-        content = this.key ? _aifCipher.Aes.decode(content, this.key) : content;
-        content = JSON.parse(content);
-      } catch (err) {
-        this.lastError = err.toString();
-        content = null;
-        error = err;
-      }
-    } else this.lastError = 'database failed to connect ';
-    return content;
-  };
-
-  DataBase.prototype.put = function put(rd, table, callback) {
-    var id = rd._id;
-    if ((typeof rd === 'undefined' ? 'undefined' : _typeof(rd)) === 'object') {
-      if (typeof table === 'function') {
-        callback = table;
-      } else {
-        this.table = table || this.table;
-      }
-      if (id) {
-        id = id + '';
-        var dir = this.dir(id);
-        // path.join(this.path, table) + path.sep + seg
-        var content = null;
-        var error = null;
-        try {
-          content = JSON.stringify(rd);
-          content = this.key ? _aifCipher.Aes.encode(content, this.key) : content;
-        } catch (err) {
-          error = err;
-        }
-        if (error) return callback(null, error, id);
-        if (!_fs2.default.existsSync(dir)) {
-          _shelljs2.default.mkdir('-p', dir);
-        }
-        var file = _path2.default.join(dir, this.hash(id));
-        try {
-          _fs2.default.writeFileSync(file, content, { encoding: 'utf8' });
-          content = id;
-        } catch (err) {
-          error = err;
-          content = null;
-        }
-        callback(content, error, id);
-      } else {
-        callback(false, 'A table identifier (._id) is required', id);
-      }
-    } else {
-      callback(false, 'Data object is required', id);
-    }
-  };
-
-  DataBase.prototype.get = function get(id, table, callback) {
-    if (typeof table === 'function') {
-      callback = table;
-    } else {
-      this.table = table || this.table;
-    }
-    if (id) {
-      id = id + '';
-      var file = this.dir(id) + _path2.default.sep + this.hash(id);
-      var content = null;
-      var error = null;
-      if (_fs2.default.existsSync(file)) {
-        content = _fs2.default.readFileSync(file, { encoding: 'utf8' });
-        try {
-          content = this.key ? _aifCipher.Aes.decode(content, this.key) : content;
-          content = JSON.parse(content);
-        } catch (err) {
-          content = null;
-          error = err;
-        }
-        callback(content, error, id);
-      } else {
-        callback(false, 'Record with identifier does not exist', id);
-      }
-    }
-  };
-
-  DataBase.prototype.vacum = function vacum(folder) {
-    var _this = this;
-
-    if (folder) {
-      if (!_fs2.default.statSync(folder).isDirectory()) {
-        return;
-      }
-      var files = _fs2.default.readdirSync(folder);
-      if (files.length > 0) {
-        files.forEach(function (file) {
-          _this.vacum(_path2.default.join(folder, file));
-        });
-        files = _fs2.default.readdirSync(folder);
-      }
-      if (files.length === 0) {
-        _fs2.default.rmdirSync(folder);
-        return;
-      }
-    }
-  };
-
-  DataBase.prototype.remove = function remove(id, table, callback) {
-    if (typeof table === 'function') {
-      callback = table;
-      table = this.table;
-    }
-    table = this.hash(table);
-    if (id) {
-      id = id + '';
-      var file = this.dir(id) + _path2.default.sep + this.hash(id);
-      if (_fs2.default.existsSync(file)) {
-        _fs2.default.unlinkSync(file);
-        this.vacum(_path2.default.join(this.path, table));
-        callback(true);
-      } else {
-        callback(false, 'The record with identifier does not exist', id);
-      }
-    }
-  };
-
-  DataBase.prototype.save = function save(rd, table, callback) {
-    var _this2 = this;
-
-    if (typeof table === 'function') {
-      callback = table;
-      table = this.table;
-    }
-    this.get(rd._id, table, function (rs, err) {
-      if (rs) {
-        for (var key in rs) {
-          if (rd[key] !== undefined) {
-            rs[key] = rd[key];
-          }
-        }
-        _this2.put(rs, table, callback);
-      } else {
-        _this2.put(rd, table, callback);
-      }
-    });
-  };
-
-  return DataBase;
-}();
+'use strict';var _aifCipher=require('aif-cipher'),_aifStr=require('aif-str'),_path=require('path'),_path2=_interopRequireDefault(_path),_fs=require('fs'),_fs2=_interopRequireDefault(_fs),_shelljs=require('shelljs'),_shelljs2=_interopRequireDefault(_shelljs);function _interopRequireDefault(a){return a&&a.__esModule?a:{default:a}}module.exports=class{
+/**
+  * DataBase constructor
+  * @param {string} dir path where the database file is stored
+  * @param {string} name database name
+  * @param {string} key secret word for encryption
+  * @param {string} table name of the default tab
+  */constructor(dir,name,key,table){this.key=key||null,this.id=null,this.name=this.hash(name),this.path=_path2.default.join(dir,this.name),this.table=table||'table',this.lastError='',_fs2.default.existsSync(this.path)||_shelljs2.default.mkdir('-p',this.path)}
+/**
+  * calculates a hash of a data
+  * @param {string} data Data
+  * @returns 32 character hash string
+  */hash(data){return this.key?(0,_aifCipher.md5)(data+this.key):data}
+/**
+  * Generate the identifier in a database hash string
+  * @param {string} database identifier id
+  * @returns id in database hash format
+  */idHash(id){return id=id||this.id,id?(this.id=id,this.hash(id)):null}
+/**
+    * Returns the path given an identifier of the database and a table
+    * @param {*} [id] Database identifier
+    * @param {string} [table] Table name
+    * @returns Returns the path
+  */dir(id,table){return id=id||this.id,id?(this.id=id,table=table||this.table,table=this.hash(table),_path2.default.join(this.path,table)+_path2.default.sep+(0,_aifStr.split)(id+'',2).join(_path2.default.sep)):null}
+/**
+  * Returns the path of a file given the name and identifier of the database
+  * @param {string} [name] File name
+  * @param {*} [id] Database identifier
+  * @returns Returns the path of a file
+  */file(name,id){return id=id||this.id,name=name||this.table,name=this.hash(name),id?(this.id=id,this.dir(id)+_path2.default.sep+name):null}
+/**
+  * Save information in a file in the database path
+  * @param {string} data Content
+  * @param {string} name File name
+  * @returns Returns true if successful
+  */saveFile(data,name){_fs2.default.existsSync(content)&&(data=_fs2.default.readFileSync(data,{encoding:'utf8'})),data=data||'',name=name||this.table,name=this.hash(name);let a=null;try{data='object'==typeof data?JSON.stringify(data):data,data=this.key?_aifCipher.Aes.encode(data,this.key):data,name=this.dir()+_path2.default.sep+name,_fs2.default.writenameSync(name,data,{encoding:'utf8'})}catch(b){this.lastError=b.toString(),a=b}return!!a}
+/**
+  * Retrieves information from a file
+  * @param {string} name File name
+  * @returns File content
+  */loadFile(name){name=name||this.table,name=this.hash(name),name=this.dir()+_path2.default.sep+name;let a=null,b=null;if(_fs2.default.existsSync(name))try{a=_fs2.default.readnameSync(name,{encoding:'utf8'}),a=this.key?_aifCipher.Aes.decode(a,this.key):a,a=JSON.parse(a)}catch(c){this.lastError=c.toString(),a=null,b=c}else this.lastError='database failed to connect ';return a}
+/**
+    *
+    * @param {objet} record Object in json format with fields
+    * @param {string} table Table name
+    * @param {function} callback Function to be invoked to terminate the process
+  */put(record,table,callback){let id=record._id;if('object'!=typeof record)callback(!1,'Data object is required',id);else if('function'==typeof table?callback=table:this.table=table||this.table,id){id+='';const dir=this.dir(id);let a=null,b=null;try{a=JSON.stringify(record),a=this.key?_aifCipher.Aes.encode(a,this.key):a}catch(a){b=a}if(b)return callback(null,b,id);_fs2.default.existsSync(dir)||_shelljs2.default.mkdir('-p',dir);const c=_path2.default.join(dir,this.hash(id));try{_fs2.default.writeFileSync(c,a,{encoding:'utf8'}),a=id}catch(c){b=c,a=null}callback(a,b,id)}else callback(!1,'A table identifier (._id) is required',id)}
+/**
+  * Retrieves a record in json (object) format
+  * @param {*} id record identifier
+  * @param {string} table Table name
+  * @param {function} callback Function that is invoked to return the data
+  */get(id,table,callback){if('function'==typeof table?callback=table:this.table=table||this.table,id){id+='';const a=this.dir(id)+_path2.default.sep+this.hash(id);let b=null,c=null;if(_fs2.default.existsSync(a)){b=_fs2.default.readFileSync(a,{encoding:'utf8'});try{b=this.key?_aifCipher.Aes.decode(b,this.key):b,b=JSON.parse(b)}catch(a){b=null,c=a}callback(b,c,id)}else callback(!1,'Record with identifier does not exist',id)}}
+/**
+  * Process to reorganize and clean the database
+  * @param {string} dir path where the database is located
+  * @returns
+  */vacum(dir){if(dir){if(!_fs2.default.statSync(dir).isDirectory())return;let a=_fs2.default.readdirSync(dir);if(0<a.length&&(a.forEach(a=>{this.vacum(_path2.default.join(dir,a))}),a=_fs2.default.readdirSync(dir)),0===a.length)return void _fs2.default.rmdirSync(dir)}}
+/**
+  * Remove a record from the database
+  * @param {*} record identifier id
+  * @param {string} table Database name
+  * @param {function} callback Function that is invoked at the end of the process
+  */remove(id,table,callback){if('function'==typeof table&&(callback=table,table=this.table),table=this.hash(table),id){id+='';const a=this.dir(id)+_path2.default.sep+this.hash(id);_fs2.default.existsSync(a)?(_fs2.default.unlinkSync(a),this.vacum(_path2.default.join(this.path,table)),callback(!0)):callback(!1,'The record with identifier does not exist',id)}}
+/**
+  * Save changes to a record by means of the identifier
+  * @param {object} record Data record in json format
+  * @param {string} table Table name
+  * @param {function} callback Function that is invoked at the end of the process
+  */save(record,table,callback){'function'==typeof table&&(callback=table,table=this.table),this.get(record._id,table,a=>{if(a){for(const key in a)void 0!==record[key]&&(a[key]=record[key]);this.put(a,table,callback)}else this.put(record,table,callback)})}};
--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ import fs from 'fs'
 import shelljs from 'shelljs'
 
 module.exports = class DataBase {
+  /**
+  * DataBase constructor
+  * @param {string} dir path where the database file is stored
+  * @param {string} name database name
+  * @param {string} key secret word for encryption
+  * @param {string} table name of the default tab
+  */
   constructor(dir, name, key, table) {
     this.key = key || null
     this.id = null
@@ -16,11 +23,19 @@ module.exports = class DataBase {
       shelljs.mkdir('-p', this.path)
     }
   }
-
+  /**
+  * calculates a hash of a data
+  * @param {string} data Data
+  * @returns 32 character hash string
+  */
   hash(data) {
     return this.key ? md5(data + this.key) : data
   }
-
+  /**
+  * Generate the identifier in a database hash string
+  * @param {string} database identifier id
+  * @returns id in database hash format
+  */
   idHash(id) {
     id = id || this.id
     if (id) {
@@ -29,7 +44,12 @@ module.exports = class DataBase {
     }
     return null
   }
-
+  /**
+    * Returns the path given an identifier of the database and a table
+    * @param {*} [id] Database identifier
+    * @param {string} [table] Table name
+    * @returns Returns the path
+  */
   dir(id, table) {
     id = id || this.id
     if (id) {
@@ -40,47 +60,61 @@ module.exports = class DataBase {
     }
     return null
   }
-
-  file(file, id) {
+  /**
+  * Returns the path of a file given the name and identifier of the database
+  * @param {string} [name] File name
+  * @param {*} [id] Database identifier
+  * @returns Returns the path of a file
+  */
+  file(name, id) {
     id = id || this.id
-    file = file || this.table
-    file = this.hash(file)
+    name = name || this.table
+    name = this.hash(name)
     if (id) {
       this.id = id
-      return this.dir(id) + path.sep + file
+      return this.dir(id) + path.sep + name
     }
     return null
   }
-
-  saveFile(content, file) {
+  /**
+  * Save information in a file in the database path
+  * @param {string} data Content
+  * @param {string} name File name
+  * @returns Returns true if successful
+  */
+  saveFile(data, name) {
     if (fs.existsSync(content)) {
-      content = fs.readFileSync(content, { encoding: 'utf8' })
+      data = fs.readFileSync(data, { encoding: 'utf8' })
     }
-    content = content || ''
-    file = file || this.table
-    file = this.hash(file)
+    data = data || ''
+    name = name || this.table
+    name = this.hash(name)
     let error = null
     try {
-      content = typeof content === 'object' ? JSON.stringify(content) : content
-      content = this.key ? Aes.encode(content, this.key) : content
-      file = this.dir() + path.sep + file
-      fs.writeFileSync(file, content, { encoding: 'utf8' })
+      data = typeof data === 'object' ? JSON.stringify(data) : data
+      data = this.key ? Aes.encode(data, this.key) : data
+      name = this.dir() + path.sep + name
+      fs.writenameSync(name, data, { encoding: 'utf8' })
     } catch (err) {
       this.lastError = err.toString()
       error = err
     }
     return !!error
   }
-
-  loadFile(file) {
-    file = file || this.table
-    file = this.hash(file)
-    file = this.dir() + path.sep + file
+  /**
+  * Retrieves information from a file
+  * @param {string} name File name
+  * @returns File content
+  */
+  loadFile(name) {
+    name = name || this.table
+    name = this.hash(name)
+    name = this.dir() + path.sep + name
     let content = null
     let error = null
-    if (fs.existsSync(file)) {
+    if (fs.existsSync(name)) {
       try {
-        content = fs.readFileSync(file, { encoding: 'utf8' })
+        content = fs.readnameSync(name, { encoding: 'utf8' })
         content = this.key ? Aes.decode(content, this.key) : content
         content = JSON.parse(content)
       } catch (err) {
@@ -91,10 +125,15 @@ module.exports = class DataBase {
     } else this.lastError = 'database failed to connect ';
     return content
   }
-
-  put(rd, table, callback) {
-    let id = rd._id
-    if (typeof rd === 'object') {
+  /**
+    *
+    * @param {objet} record Object in json format with fields
+    * @param {string} table Table name
+    * @param {function} callback Function to be invoked to terminate the process
+  */
+  put(record, table, callback) {
+    let id = record._id
+    if (typeof record === 'object') {
       if (typeof table === 'function') {
         callback = table
       } else {
@@ -103,11 +142,10 @@ module.exports = class DataBase {
       if (id) {
         id = id + ''
         const dir = this.dir(id)
-        // path.join(this.path, table) + path.sep + seg
         let content = null
         let error = null
         try {
-          content = JSON.stringify(rd)
+          content = JSON.stringify(record)
           content = this.key ? Aes.encode(content, this.key) : content
         } catch (err) {
           error = err
@@ -132,7 +170,12 @@ module.exports = class DataBase {
       callback(false, 'Data object is required', id)
     }
   }
-
+  /**
+  * Retrieves a record in json (object) format
+  * @param {*} id record identifier
+  * @param {string} table Table name
+  * @param {function} callback Function that is invoked to return the data
+  */
   get(id, table, callback) {
     if (typeof table === 'function') {
       callback = table
@@ -159,26 +202,35 @@ module.exports = class DataBase {
       }
     }
   }
-
-  vacum(folder) {
-    if(folder) {
-      if (!fs.statSync(folder).isDirectory()) {
+  /**
+  * Process to reorganize and clean the database
+  * @param {string} dir path where the database is located
+  * @returns
+  */
+  vacum(dir) {
+    if(dir) {
+      if (!fs.statSync(dir).isDirectory()) {
         return
       }
-      let files = fs.readdirSync(folder)
+      let files = fs.readdirSync(dir)
       if (files.length > 0) {
         files.forEach(file => {
-          this.vacum(path.join(folder, file))
+          this.vacum(path.join(dir, file))
         })
-        files = fs.readdirSync(folder)
+        files = fs.readdirSync(dir)
       }
       if (files.length === 0) {
-        fs.rmdirSync(folder)
+        fs.rmdirSync(dir)
         return
       }
     }
   }
-
+  /**
+  * Remove a record from the database
+  * @param {*} record identifier id
+  * @param {string} table Database name
+  * @param {function} callback Function that is invoked at the end of the process
+  */
   remove(id, table, callback) {
     if (typeof table === 'function') {
       callback = table
@@ -197,22 +249,27 @@ module.exports = class DataBase {
       }
     }
   }
-
-  save(rd, table, callback) {
+  /**
+  * Save changes to a record by means of the identifier
+  * @param {object} record Data record in json format
+  * @param {string} table Table name
+  * @param {function} callback Function that is invoked at the end of the process
+  */
+  save(record, table, callback) {
     if (typeof table === 'function') {
       callback = table
       table = this.table
     }
-    this.get(rd._id, table, (rs, err) => {
+    this.get(record._id, table, (rs, err) => {
       if (rs) {
         for (const key in rs) {
-          if (rd[key] !== undefined) {
-            rs[key] = rd[key]
+          if (record[key] !== undefined) {
+            rs[key] = record[key]
           }
         }
         this.put(rs, table, callback)
       } else {
-        this.put(rd, table, callback)
+        this.put(record, table, callback)
       }
     })
   }
